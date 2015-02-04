a2c322be1ae5c6aa4f8a80dbe54b366b9e26e8be
Igor Babuschkin <igor@babuschk.in>
2015-02-04 11:22:55 +0100

    Implement curly brace expansion and bring API more line with read_hdf

--- root_pandas/__init__.py
+++ root_pandas/__init__.py
@@ -7,23 +7,53 @@ from pandas import DataFrame
 from root_numpy import root2array, list_trees
 from fnmatch import fnmatch
 from root_numpy import list_branches
+import itertools
 from math import ceil
+import re
 import ROOT
 
 __all__ = ['read_root']
 
-def get_matching_variables(fname, tree, patterns):
-    branches = list_branches(fname, tree)
+def expand_braces(orig):
+    r = r'.*(\{.+?[^\\]\})'
+    p = re.compile(r)
+
+    s = orig[:]
+    res = list()
+
+    m = p.search(s)
+    if m is not None:
+        sub = m.group(1)
+        open_brace = s.find(sub)
+        close_brace = open_brace + len(sub) - 1
+        if sub.find(',') != -1:
+            for pat in sub.strip('{}').split(','):
+                res.extend(expand_braces(s[:open_brace] + pat + s[close_brace+1:]))
+
+        else:
+            res.extend(expand_braces(s[:open_brace] + sub.replace('}', '\\}') + s[close_brace+1:]))
+
+    else:
+        res.append(s.replace('\\}', '}'))
+
+    return list(set(res))
+
+def get_matching_variables(branches, patterns):
 
     selected = []
 
     for p in patterns:
+        found = False
         for b in branches:
+            if fnmatch(b, p):
+                found = True
             if fnmatch(b, p) and not b in selected:
                 selected.append(b)
+        if not found:
+            raise ValueError("Pattern '{}' didn't match any branch".format(p))
     return selected
 
-def read_root(fname, tree_name=None, variables=None, ignore=None, chunksize=None, *kargs, **kwargs):
+def read_root(path, tree_key=None, columns=None, ignore=None, chunksize=None, where=None, *kargs, **kwargs):
     """
     Read a ROOT file into a pandas DataFrame.
     Further *kargs and *kwargs are passed to root_numpy's root2array.
@@ -31,25 +61,25 @@ def read_root(fname, tree_name=None, variables=None, ignore=None, chunksize=None
 
     Parameters
     ----------
-    fname: string
-        The filename of the root file
-    tree_name: string
-        The name of the tree to load
-    variables: sequence
-        A sequence of shell-patterns. Matching variables are read.
-    ignore: sequence
-        A sequence of shell-patterns. All matching variables are ignored (overriding the variables argument)
+    path: string
+        The path to the root file
+    tree_key: string
+        The key of the tree to load.
+    columns: sequence of str
+        A sequence of shell-patterns (can contain *, ?, [] or {}). Matching columns are read.
+    ignore: sequence of str
+        A sequence of shell-patterns (can contain *, ?, [] or {}). All matching columns are ignored (overriding the columns argument)
     chunksize: int
-        If this parameter is specified, an iterator is returned that yields DataFrames with chunksize rows
+        If this parameter is specified, an iterator is returned that yields DataFrames with `chunksize` rows
 
     Returns
     -------
-        DataFrame from the ROOT file
+        DataFrame created from matching data in the specified TTree
 
     Notes
     -----
 
-        >>> df = read_root('test.root', 'MyTree', variables=['x_*', 'y_*'], selection='x_1 > 100')
+        >>> df = read_root('test.root', 'MyTree', columns=['A{B,C}*', 'D'], where='ABB > 100')
 
     """
     if not tree_name:
@@ -59,18 +89,22 @@ def read_root(fname, tree_name=None, variables=None, ignore=None, chunksize=None
         else:
             raise ValueError('More than one tree found in {}'.format(fname))
 
-    if not variables:
+    branches = list_branches(fname, tree_name)
+
+    if not columns:
         all_vars = None
     else:
         # index is always loaded if it exists
-        variables.append('index')
-        all_vars = get_matching_variables(fname, tree_name, variables)
+        if 'index' in branches:
+            columns = columns[:]
+            columns.append('index')
+        columns = list(itertools.chain.from_iterable(map(expand_braces, columns)))
+        all_vars = get_matching_variables(branches, columns)
+        print(all_vars)
 
     if ignore:
-        if not all_vars:
-            all_vars = get_matching_variables(fname, tree_name, ['*'])
-
-        ignored = get_matching_variables(fname, tree_name, ignore)
+        ignored = get_matching_variables(branches, ignore)
+        ignored = list(itertools.chain.from_iterable(map(expand_braces, ignored)))
         if 'index' in ignored:
             raise ValueError('index variable is being ignored!')
         for var in ignored:
@@ -82,11 +116,11 @@ def read_root(fname, tree_name=None, variables=None, ignore=None, chunksize=None
         f.Close()
         def genchunks():
             for chunk in range(int(ceil(float(n_entries) / chunksize))):
-                arr = root2array(fname, tree_name, all_vars, start=chunk * chunksize, stop=(chunk+1) * chunksize, *kargs, **kwargs)
+                arr = root2array(fname, tree_name, all_vars, start=chunk * chunksize, stop=(chunk+1) * chunksize, selection=where, *kargs, **kwargs)
                 yield convert_to_dataframe(arr)
         return genchunks()
 
-    arr = root2array(fname, tree_name, all_vars, *kargs, **kwargs)
+    arr = root2array(fname, tree_name, all_vars, selection=where, *kargs, **kwargs)
     return convert_to_dataframe(arr)
 
 def convert_to_dataframe(array):
