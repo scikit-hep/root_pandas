5be688662bb943887feed465b88b319be25bdf66
Igor Babuschkin <igor@babuschk.in>
2016-02-22 15:43:02 +0000

    Change version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='root_pandas',
-      version='0.1.0',
+      version='0.1.1',
       description='Read and save DataFrames from and to ROOT files',
       url='http://github.com/ibab/root_pandas',
       author='Igor Babuschkin',
