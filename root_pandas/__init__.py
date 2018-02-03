bae82d5e4bc7ece43fce12640967149996d4ee61
Christopher Burr <christopher.burr@cern.ch>
2018-02-03 15:59:11 +0100

    Add __version__ attribute and use it with pypi deployments

--- root_pandas/__init__.py
+++ root_pandas/__init__.py
@@ -1,7 +1,9 @@
 from .readwrite import read_root
 from .readwrite import to_root
+from .version import __version__
 
 __all__ = [
+    '__version__',
     'read_root',
     'to_root',
 ]
--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+import os
+
 from setuptools import setup
 
+
+def get_version():
+    g = {}
+    exec(open(os.path.join("root_pandas", "version.py")).read(), g)
+    return g["__version__"]
+
+
 setup(name='root_pandas',
-      version='0.2.0',
+      version=get_version(),
       description='Read and save DataFrames from and to ROOT files',
       url='http://github.com/chrisburr/root_pandas',
       author='Chris Burr',
